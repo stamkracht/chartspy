26ff61f65827c0daec804f765f1a71e3cb0e2677
Roel Obdam <roel@purewebsolutions.nl>
2016-05-01 21:12:49 +0200

    Fix import for py3 and remove py 2.6 from test

--- tests/chartspy_tests.py
+++ tests/chartspy_tests.py
@@ -1,7 +1,7 @@
 import hashlib
 import os
 import random
-from StringIO import StringIO
+from io import StringIO
 
 from nose.tools import *
 
@@ -12,19 +12,19 @@ from chartspy.svg import SVG
 def test_basic_bar_chart():
     svg = SVG(800, 600)
 
-
     random.seed('8==D~')
     data = [{
-                'value': x+1,
+                'value': x + 1,
                 'label': str('label {}').format(x),
-                'fill_color': "#{}".format('ff0000' if x%2 == 0 else '0000ff')
+                'fill_color': "#{}".format('ff0000' if x % 2 == 0 else '0000ff')
             }
             for x in range(0, 10)
-    ]
+            ]
     bar_chart = create_bar_chart(svg, data, bar_spacing=10, margin=50)
 
     stringio = StringIO()
     bar_chart.to_png(stringio)
     hex = hashlib.md5(stringio.getvalue()).hexdigest()
-    ref_hex = hashlib.md5(open(os.path.join(os.path.dirname(__file__), 'reference_bar_chart.png'), 'r').read()).hexdigest()
+    ref_hex = hashlib.md5(
+        open(os.path.join(os.path.dirname(__file__), 'reference_bar_chart.png'), 'r').read()).hexdigest()
     eq_(ref_hex, hex)
